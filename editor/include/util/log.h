70b44ae925aa81ca9767f612d2a19e6a49c48621
Potcharapoll <watanabemasaya@outlook.com>
2024-08-20 23:41:37 +0700

    Remove editor

--- editor/include/util/log.h
+++
@@ -1,7 +0,0 @@
-#ifndef LOG_H
-#define LOG_H
-#define LOG_DEBUG(...) fprintf(stdout, "[DEBUG]: "); fprintf(stdout, __VA_ARGS__); putchar('\n');
-#define LOG_EVENT(...) fprintf(stdout, "[EVENT]: "); fprintf(stdout, __VA_ARGS__); putchar('\n');
-#define LOG_ERROR(...) fprintf(stderr, "[ERROR]: "); fprintf(stderr,  __VA_ARGS__); putchar('\n');
-#define LOG_FETAL(...) fprintf(stderr, "[FETAL_ERROR]: "); fprintf(stderr,  __VA_ARGS__); putchar('\n');
-#endif
